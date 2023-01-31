ea09facd80557be03cbd211953b1826a8a80e819
Charlie Denton <charlie@meshy.co.uk>
2023-01-31 22:29:03 +0000

    Update docstring
    The summary returned is no longer JSON.
    
    Co-authored-by: Jeppe Fihl-Pearson <jeppe@memrise.com>

--- mypy_json_report.py
+++ mypy_json_report.py
@@ -72,7 +72,7 @@ class MypyError:
 
 
 def parse_errors_report(input_lines: Iterator[str]) -> Dict[str, Dict[str, int]]:
-    """Given lines from mypy's output, return a JSON summary of error frequencies by file."""
+    """Given lines from mypy's output, return a summary of error frequencies by file."""
     errors = _extract_errors(input_lines)
     error_frequencies = _count_errors(errors)
     structured_errors = _structure_errors(error_frequencies)
